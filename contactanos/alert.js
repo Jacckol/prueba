daa95e96b362950476f042e4712bd0799e723f16
Jacckol <ajp.mendoza@yavirac.edu.ec>
2023-02-03 20:55:28 -0500

    alert

---
+++ contactanos/alert.js
@@ -0,0 +1,60 @@
+Swal.fire({
+    title:"Unete a nosotros",
+    text: 'desea llenar el formulario',
+   // html:
+    icon:'question',
+    confirmButtonText:'ok'
+   // footer:
+   // width:
+   // padding:
+   // background:
+   // grow:
+   // backdrop:
+   // timer:
+   // timerProgressBar:
+   // toast:
+   // position:
+   // allowOutsideClick:
+   // allowEscapeKey:
+   // allowEnterKey:
+   // stopKeydownPropagation:
+
+   // input:
+   // inputPlaceholder:
+   // inputValue:
+   // inputOptions:
+   
+   //  customClass:
+   // 	container:
+   // 	popup:
+   // 	header:
+   // 	title:
+   // 	closeButton:
+   // 	icon:
+   // 	image:
+   // 	content:
+   // 	input:
+   // 	actions:
+   //confirmButton:
+   // 	cancelButton:
+   // 	footer:	
+
+   // showConfirmButton:true
+   // confirmButtonColor:'#eE60E9'
+   // confirmButtonAriaLabel:'cconfirmar'
+
+   // showCancelButton:
+   // cancelButtonText:
+   // cancelButtonColor:
+   // cancelButtonAriaLabel:
+   
+   // buttonsStyling:
+   // showCloseButton:
+   // closeButtonAriaLabel:
+
+
+   // imageUrl:
+   // imageWidth:
+   // imageHeight:
+   // imageAlt:
+});
